2f3a1c1fb8116d21d27a5f48ea5ce023f471853e
User <user@example.com>
2026-09-05 20:59:54 -0600

    Refresh of fork-workspace-file-uploads

--- packages/contracts/src/rpc.ts
+++ packages/contracts/src/rpc.ts
@@ -149,6 +149,9 @@ import {
   ProjectSearchEntriesError,
   ProjectSearchEntriesInput,
   ProjectSearchEntriesResult,
+  ProjectUploadFileError,
+  ProjectUploadFileInput,
+  ProjectUploadFileResult,
   ProjectWriteFileError,
   ProjectWriteFileInput,
   ProjectWriteFileResult,
@@ -248,6 +251,7 @@ export const WS_METHODS = {
   projectsReadFile: "projects.readFile",
   projectsSearchContents: "projects.searchContents",
   projectsSearchEntries: "projects.searchEntries",
+  projectsUploadFile: "projects.uploadFile",
   projectsWriteFile: "projects.writeFile",
 
   // Shell methods
@@ -849,6 +853,12 @@ export const WsProjectsWriteFileRpc = Rpc.make(WS_METHODS.projectsWriteFile, {
   error: Schema.Union([ProjectWriteFileError, EnvironmentAuthorizationError]),
 });
 
+export const WsProjectsUploadFileRpc = Rpc.make(WS_METHODS.projectsUploadFile, {
+  payload: ProjectUploadFileInput,
+  success: ProjectUploadFileResult,
+  error: Schema.Union([ProjectUploadFileError, EnvironmentAuthorizationError]),
+});
+
 export const WsShellOpenInEditorRpc = Rpc.make(WS_METHODS.shellOpenInEditor, {
   payload: LaunchEditorInput,
   error: Schema.Union([ExternalLauncherError, EnvironmentAuthorizationError]),
@@ -1314,6 +1324,7 @@ export const WsRpcGroup = RpcGroup.make(
   WsProjectsReadFileRpc,
   WsProjectsSearchContentsRpc,
   WsProjectsSearchEntriesRpc,
+  WsProjectsUploadFileRpc,
   WsProjectsWriteFileRpc,
   WsShellOpenInEditorRpc,
   WsFilesystemBrowseRpc,
