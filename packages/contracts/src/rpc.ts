cbd6757595c80328cb162b99d690c1eff662e318
User <user@example.com>
2026-09-05 21:03:49 -0600

    feat(web): upload files into workspace via drag-and-drop

--- packages/contracts/src/rpc.ts
+++ packages/contracts/src/rpc.ts
@@ -165,6 +165,9 @@ import {
   ProjectSearchEntriesError,
   ProjectSearchEntriesInput,
   ProjectSearchEntriesResult,
+  ProjectUploadFileError,
+  ProjectUploadFileInput,
+  ProjectUploadFileResult,
   ProjectWriteFileError,
   ProjectWriteFileInput,
   ProjectWriteFileResult,
@@ -289,6 +292,7 @@ export const WS_METHODS = {
   projectsReadFile: "projects.readFile",
   projectsSearchContents: "projects.searchContents",
   projectsSearchEntries: "projects.searchEntries",
+  projectsUploadFile: "projects.uploadFile",
   projectsWriteFile: "projects.writeFile",
 
   // Shell methods
@@ -975,6 +979,12 @@ const WsProjectsWriteFileRpc = Rpc.make(WS_METHODS.projectsWriteFile, {
   error: Schema.Union([ProjectWriteFileError, EnvironmentAuthorizationError]),
 });
 
+const WsProjectsUploadFileRpc = Rpc.make(WS_METHODS.projectsUploadFile, {
+  payload: ProjectUploadFileInput,
+  success: ProjectUploadFileResult,
+  error: Schema.Union([ProjectUploadFileError, EnvironmentAuthorizationError]),
+});
+
 const WsShellOpenInEditorRpc = Rpc.make(WS_METHODS.shellOpenInEditor, {
   payload: LaunchEditorInput,
   error: Schema.Union([ExternalLauncherError, EnvironmentAuthorizationError]),
@@ -1509,6 +1519,7 @@ export const WsRpcGroup = RpcGroup.make(
   WsProjectsReadFileRpc,
   WsProjectsSearchContentsRpc,
   WsProjectsSearchEntriesRpc,
+  WsProjectsUploadFileRpc,
   WsProjectsWriteFileRpc,
   WsShellOpenInEditorRpc,
   WsFilesystemBrowseRpc,
