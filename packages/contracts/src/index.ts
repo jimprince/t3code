0a1177228982911fe87382dbbb6c66cbb943a969
brad <bradprin@live.com>
2026-09-17 19:37:30 +0000

    feat(codex): surface active goals in thread status

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -2,6 +2,7 @@ export * from "./baseSchemas.ts";
 export * from "./assistantCitations.ts";
 export * from "./composerContext.ts";
 export * from "./composerContextClipboard.ts";
+export * from "./codexNativeGoal.ts";
 export * from "./background.ts";
 export * from "./auth.ts";
 export * from "./environment.ts";
